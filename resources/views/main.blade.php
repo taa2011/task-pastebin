8b37c7da6ff206f78be1298c6de6f5bf2ae32c63
vagrant <vagrant@homestead>
2018-06-04 03:02:06 +0000

    include card in main view

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -25,12 +25,7 @@
 
             @foreach ($last10 as $pasterec)
 
-            <div class="card my-2">
-                <div class="card-body">
-                    <pre><code class="language-css">{{ $pasterec->code }}</pre></code>
-                    <a href="\{{ $pasterec->hash }}" class="card-link">{{ $pasterec->hash }}</a>
-                </div>
-            </div>
+                @include('card')
 
             @endforeach
         </div>
